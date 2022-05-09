62f53fba429424e9b5622f4362fc39a5771c9bf6
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-09 02:37:44 -0500

    configured mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import mongoDB from './config/mongodb';
+
+const app = express();
+
+// Connect to MongoDB
+mongoDB();
+
+// Use Cross-Origin-Resource-Sharing & JSON parser
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
