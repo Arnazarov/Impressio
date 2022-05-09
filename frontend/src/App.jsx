c50311560204f9098212b4fd426bdf92d89dfd26
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-09 04:52:53 -0500

    added styles for components form/post/posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import logo from './images/logo.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './appStyles';
 
 const App = () => {
+  const styles = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit"></AppBar>
+      <AppBar className={styles.appBar} position="static" color="inherit">
+        <Typography className={styles.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className={styles.image} src={logo} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
---
+++ frontend/src/components/Form/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import useStyles from './formStyles';
+
+const Form = () => {
+  const styles = useStyles();
+  return <div>Form</div>;
+};
+
+export default Form;
