7507b7a5fedf96fe393cad2e70724af5ecc23842
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-10 22:39:28 -0500

    extracted home screen from app container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 
 import Header from './components/Header/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <Container maxWidth="lg">
       <Header />
+      <HomeScreen />
     </Container>
   );
 };
