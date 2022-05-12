1ec7a4b83e6c1090a78db70c82ef722dcb47fc54
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-12 09:46:22 -0500

    modified auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,34 +1,33 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js'
 
-const protectImpressio = async (req, res, next) => {
+const protectApp = async (req, res, next) => {
 
     let token, verifiedToken;
-    try {
 
+    try {
         if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
             token = req.headers.authorization.split(' ')[1];
             const impressioLogin = token.length < 500; 
-
+            
             if (token && impressioLogin) {
                 verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
                 req.user = await User.findById(verifiedToken.id).select('-password');
+                next();
             } else if (!impressioLogin) {
                 verifiedToken = jwt.decode(token);
                 req.user = verifiedToken;
+                next();
             }
-
-            next();
         }
 
+        
         if (!token) {
-            res.status(401).json({message: 'Not authorized -- token failed'})
+            res.status(401).json({message: 'Not authorized -- token failed'}) 
         }
-
-
     } catch(err) {
         console.log(err);
     }
 } 
 
-export default protectImpressio;
+export default protectApp;
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import { fetchPosts, createPost, updatePost, deletePost, likePost } from '../controllers/postController.js';
+import protectApp from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
 router.route('/')
     .get(fetchPosts)
-    .post(createPost);
+    .post(protectApp, createPost);
 
 router.route('/:id')
-    .patch(updatePost)
-    .delete(deletePost);
+    .patch(protectApp,updatePost)
+    .delete(protectApp, deletePost);
 
 router.route('/:id/like')
-    .patch(likePost);
+    .patch(protectApp, likePost);
 
 export default router;
